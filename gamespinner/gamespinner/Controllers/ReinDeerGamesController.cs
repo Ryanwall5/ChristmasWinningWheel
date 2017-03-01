d6a79e715a50578397ce52baee587bfb859e1b0d
ryanwall5 <ryanwall5@yahoo.com>
2017-03-01 14:36:14 -0800

    first commit for christmas wheel game i made over christmas break

---
+++ gamespinner/gamespinner/Controllers/ReinDeerGamesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace gamespinner.Controllers
+{
+    public class ReinDeerGamesController : Controller
+    {
+        // GET: ReinDeerGames
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
